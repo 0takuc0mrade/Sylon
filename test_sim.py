62af3fb338a44250728ecb0ffa55e1ca4710a686
Hillary Ikhais <ikhaisoshuare@Hillarys-MacBook-Pro.local>
2026-06-04 20:14:14 +0100

    added fivetran mcp

--- test_sim.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-from openserv.orchestrator import run_simulation
-print(run_simulation("what if I change my menu?", "test_123"))
